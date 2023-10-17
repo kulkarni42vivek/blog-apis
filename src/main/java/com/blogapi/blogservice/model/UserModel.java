015ff9fe70fb642a3e15b3878e975d276a4a6079
vivek <kulkarni42vivek@gmail.com>
2023-10-17 23:52:03 +0530

    asdf

--- src/main/java/com/blogapi/blogservice/model/UserModel.java
+++ src/main/java/com/blogapi/blogservice/model/UserModel.java
@@ -3,12 +3,6 @@ package com.blogapi.blogservice.model;
 
 import java.sql.Timestamp;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,21 +10,14 @@ import lombok.NoArgsConstructor;
 
 @Builder
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "user_mstr" , uniqueConstraints = @UniqueConstraint(columnNames = "email" , name = "email_constraint") )
+@NoArgsConstructor
 public class UserModel {
-	@Id
 	private String userId ;
 	private String password ;
 	private String firstName ;
 	private String lastName ;
-	
-	//@Column(name="mobile" , unique = true)
 	private String mobile ;
-	
-	//@Column(name = "email", nullable = false )
 	private String email ;
 	private Timestamp createOn;
 	private String userProfilePath ;
--- src/main/java/com/blogapi/blogservice/repo/impl/LoginDaoImpl.java
+++ src/main/java/com/blogapi/blogservice/repo/impl/LoginDaoImpl.java
@@ -2,13 +2,16 @@ package com.blogapi.blogservice.repo.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.blogapi.blogservice.Util.Constants;
 import com.blogapi.blogservice.configuration.DataSource2Configuration;
 import com.blogapi.blogservice.configuration.QueryMaster;
+import com.blogapi.blogservice.model.GenreModel;
 import com.blogapi.blogservice.model.ResponseMessage;
 import com.blogapi.blogservice.model.UserModel;
 import com.blogapi.blogservice.model.UserModelDTO;
@@ -25,7 +28,6 @@ public class LoginDaoImpl implements LoginDao {
 		int[] resultRecords = null;
 		StringBuilder query = new StringBuilder();
 		PreparedStatement pStmt = null;
-		int k =0 ;
 		try {
 			query.append("INSERT INTO user_genre_mapping values(user_id , genre) values (?,?)");
 
@@ -35,8 +37,7 @@ public class LoginDaoImpl implements LoginDao {
 				int i = 0;
 
 				pStmt.setString(++i, req.getUserId());
-				pStmt.setString(++i, req.getGenre().get(k));
-				k++;
+				pStmt.setString(++i, req.getGenre().get(j));
 				pStmt.addBatch();
 			}
 			resultRecords = pStmt.executeBatch();
@@ -63,19 +64,98 @@ public class LoginDaoImpl implements LoginDao {
 
 	@Override
 	public ResponseMessage register(UserModel user) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		QueryMaster qm = new QueryMaster();
+		StringBuilder query = new StringBuilder();
+		ArrayList<Object> params = new ArrayList<>();
+		ResponseMessage response = new ResponseMessage();
+		int result = 0;
+		try {
+			con = datasource.getMasterDBConnection();
+			query.append("insert into user_mstr (user_id ,password , first_name , last_name ,mobile , email, created_on )");
+			
+			params.add(user.getUserId());
+			params.add(user.getPassword());
+			params.add(user.getFirstName());
+			params.add(user.getLastName());
+			params.add(user.getMobile());
+			params.add(user.getEmail());
+			params.add(user.getCreateOn());
+			query.append("values (?,?,?,?,?,?,?) ;");
+			
+			result = qm.updateInsert(query.toString(), params, con, null);
+			if (result == 1) {
+				response.setErrorCode(Constants.ErrorCodes.TRANSACTION_SUCCESS);
+				response.setErrorMessage("success");
+			} else {
+				response.setErrorCode(Constants.ErrorCodes.TRANSACTION_FAILED);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setErrorCode(Constants.ErrorCodes.TRANSACTION_FAILED);
+		} finally {
+			datasource.closeConnection(con);
+		}
+		return response;
 	}
 
 	@Override
 	public UserModel getUserDetails(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = null;
+		QueryMaster qm = new QueryMaster();
+		StringBuilder sb = new StringBuilder();
+		UserModel userModel = new UserModel();
+		List<Object> param = new ArrayList();
+		try {
+			conn = datasource.getMasterDBConnection();
+			
+			sb.append("select * from user_mstr where user_id = ?");
+			param.add(username);
+			
+			ResultSet res = qm.select(sb.toString(), param, conn, null);
+			if (res.next()) {
+				userModel.setUserId(res.getString("user_id"));
+				userModel.setPassword(res.getString("password"));
+			}
+			return userModel ;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			datasource.closeConnection(conn);
+		}
+		return userModel;
 	}
 
 	@Override
 	public ResponseMessage updatePassword(UserModel userModel) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Connection con = null;
+		QueryMaster qm = new QueryMaster();
+		StringBuilder query = new StringBuilder();
+		ArrayList<Object> params = new ArrayList<>();
+		ResponseMessage response = new ResponseMessage();
+		int result = 0;
+		try {
+			con = datasource.getMasterDBConnection();
+			
+			query.append("update user_mstr set password = ? where user_id =?  ");
+			params.add(userModel.getPassword());
+			params.add(userModel.getUserId());
+			
+			result = qm.updateInsert(query.toString(), params, con, null);
+			if (result == 1) {
+				response.setErrorCode(Constants.ErrorCodes.TRANSACTION_SUCCESS);
+				response.setErrorMessage("success");
+			} else {
+				response.setErrorCode(Constants.ErrorCodes.TRANSACTION_FAILED);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setErrorCode(Constants.ErrorCodes.TRANSACTION_FAILED);
+		} finally {
+			datasource.closeConnection(con);
+		}
+		return response;
+
 	}
 }
